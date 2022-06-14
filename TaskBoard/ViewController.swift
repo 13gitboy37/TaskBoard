aac05872cb47c6c6f1ba722c6f311a20f0da971e
nikitamoshentsev <trane0909@bk.ru>
2022-06-14 14:01:00 +0300

    Initial Commit

---
+++ TaskBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskBoard
+//
+//  Created by Никита Мошенцев on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
